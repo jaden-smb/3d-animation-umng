1e95221b488fb75c4ddcff6d06b71cf2b8a51457
jaden-smb <santiagomontoyabaiter@gmail.com>
2024-01-29 14:07:54 -0500

    UMNG-1-1 second-task updated, added more time

--- first_cut/stretch-and-squash-task/stretch-and-squash-ball-axis-bounding.py
+++ first_cut/stretch-and-squash-task/stretch-and-squash-ball-axis-bounding.py
@@ -9,7 +9,7 @@ cmds.setAttr(ball + '.translateY', 20)  # Start from a higher position
 # Set keyframes for the ball's Y position to simulate falling and bouncing
 # The ball will fall to the ground over the course of 1 second (24 frames at 24 fps)
 # Then it will bounce 4 times over the next 9 seconds (216 frames)
-fall_time = 24
+fall_time = 45
 bounce_times = [fall_time, fall_time + 54, fall_time + 108, fall_time + 162, fall_time + 216]
 bounce_heights = [10, 8, 6, 4, 2]
 
@@ -40,3 +40,7 @@ for time in bounce_times:
     cmds.setKeyframe(ball, attribute='scaleY', time=time+30, value=1.5)  # Stretch
     cmds.setKeyframe(ball, attribute='scaleX', time=time, value=1.2)  # Squash
     cmds.setKeyframe(ball, attribute='scaleX', time=time+30, value=0.8)  # Stretch
+    
+# Set the final keyframes for the ball's scale to return to initial size
+cmds.setKeyframe(ball, attribute='scaleY', time=bounce_times[-1]+30, value=1)  # Return to initial Y scale
+cmds.setKeyframe(ball, attribute='scaleX', time=bounce_times[-1]+30, value=1)  # Return to initial X scale
