37155b6c86db5c9be7e1a4da24a36ea52c709b6d
jaden-smb <santiagomontoyabaiter@gmail.com>
2024-01-29 07:57:23 -0500

    UMNG-1-1 first versions of tasks done

---
+++ first_cut/stretch-and-squash-task/stretch-and-squash-ball-axis-bounding.py
@@ -0,0 +1,42 @@
+import maya.cmds as cmds
+
+# Create a sphere
+ball = cmds.polySphere(r=1, name='ball')[0]
+
+# Set initial position
+cmds.setAttr(ball + '.translateY', 20)  # Start from a higher position
+
+# Set keyframes for the ball's Y position to simulate falling and bouncing
+# The ball will fall to the ground over the course of 1 second (24 frames at 24 fps)
+# Then it will bounce 4 times over the next 9 seconds (216 frames)
+fall_time = 24
+bounce_times = [fall_time, fall_time + 54, fall_time + 108, fall_time + 162, fall_time + 216]
+bounce_heights = [10, 8, 6, 4, 2]
+
+# Set keyframe for the start of the fall
+cmds.setKeyframe(ball, attribute='translateY', time=0, value=20)
+# Set keyframe for the end of the fall
+cmds.setKeyframe(ball, attribute='translateY', time=fall_time, value=0)
+
+for i in range(len(bounce_times) - 1):
+    # Set keyframe for when the ball hits the ground
+    cmds.setKeyframe(ball, attribute='translateY', time=bounce_times[i], value=0)
+    # Set keyframe for the peak of the bounce
+    peak_time = (bounce_times[i] + bounce_times[i+1]) / 2
+    cmds.setKeyframe(ball, attribute='translateY', time=peak_time, value=bounce_heights[i+1])
+
+# Set the final keyframe for when the ball hits the ground
+cmds.setKeyframe(ball, attribute='translateY', time=bounce_times[-1], value=0)
+
+# Set keyframes for the ball's X position to simulate forward motion
+# The ball will move steadily in the X direction over the course of the animation
+cmds.setKeyframe(ball, attribute='translateX', time=0, value=0)
+cmds.setKeyframe(ball, attribute='translateX', time=240, value=10)
+
+# Set keyframes for the ball's scale to simulate squash and stretch
+# The ball will squash when it hits the ground and stretch in the air
+for time in bounce_times:
+    cmds.setKeyframe(ball, attribute='scaleY', time=time, value=0.8)  # Squash
+    cmds.setKeyframe(ball, attribute='scaleY', time=time+30, value=1.5)  # Stretch
+    cmds.setKeyframe(ball, attribute='scaleX', time=time, value=1.2)  # Squash
+    cmds.setKeyframe(ball, attribute='scaleX', time=time+30, value=0.8)  # Stretch
---
+++ first_cut/stretch-and-squash-task/stretch-and-squash-ball.py
@@ -0,0 +1,40 @@
+import maya.cmds as cmds
+
+# Set the initial position of the ball
+start_pos = 10
+end_pos = 0
+ball = cmds.polySphere(r=1, sx=20, sy=20, ax=(0, 1, 0), cuv=2, ch=1)[0]
+
+# Set the total frames for the animation
+total_frames = 24 * 5  # 24 fps * 5 seconds
+extra_frames = 24 * 2  # 24 fps * 2 seconds for the exaggeration phase
+
+# Set the keyframes for the ball's position
+for i in range(0, total_frames + extra_frames):
+    time = i
+    # Calculate the current position of the ball
+    if time < total_frames:
+        pos = start_pos - ((start_pos - end_pos) * (time / float(total_frames))**2)
+    else:
+        pos = end_pos
+    # Set the ball's position at this frame
+    cmds.setKeyframe(ball, attribute='translateY', v=pos, t=time)
+
+    # Calculate the squash and stretch
+    if pos > end_pos + 1:
+        # The ball is in the air, so stretch it along the Y axis
+        stretch = 1 + (time / float(total_frames))  # Gradually increase the stretch from 1 to 2
+        cmds.setKeyframe(ball, attribute='scaleY', v=stretch, t=time)
+        cmds.setKeyframe(ball, attribute='scaleX', v=1/stretch, t=time)
+        cmds.setKeyframe(ball, attribute='scaleZ', v=1/stretch, t=time)
+    else:
+        # The ball is on the ground, so squash it along the X axis
+        if time < total_frames:
+            squash = 1 + ((total_frames - time) / float(total_frames))  # Gradually decrease the squash from 2 to 1
+        else:
+            # Exaggerate the squash for 2 seconds, then return to initial state
+            exaggeration_factor = 2  # Change this to stretch more or less
+            squash = 1 + exaggeration_factor * ((total_frames + extra_frames - time) / float(extra_frames))
+        cmds.setKeyframe(ball, attribute='scaleY', v=1/squash, t=time)
+        cmds.setKeyframe(ball, attribute='scaleX', v=squash, t=time)
+        cmds.setKeyframe(ball, attribute='scaleZ', v=squash, t=time)
