7a109230334266c8b917e4eb47b56fd873bbc95b
jaden-smb <santiagomontoyabaiter@gmail.com>
2024-02-12 10:10:02 -0500

    UMNG-1-1 implementing clean code in the first task and documentation

--- first_cut/stretch-and-squash-task/stretch-and-squash-ball-axis-bounding.py
+++ first_cut/stretch-and-squash-task/stretch-and-squash-ball-axis-bounding.py
@@ -1,90 +1,82 @@
 import maya.cmds as cmds
 
-# Create a sphere
-ball = cmds.polySphere(r=1, name='ball')[0]
+def create_ball_and_rig():
+    """Create a sphere and a group to act as the rig for the ball."""
+    ball = cmds.polySphere(r=1, name='ball')[0]
+    rig = cmds.group(empty=True, name='ball_rig')
+    cmds.parent(ball, rig)
+    ball_center = cmds.xform(ball, query=True, worldSpace=True, rotatePivot=True)
+    cmds.xform(rig, worldSpace=True, pivots=ball_center)
+    return ball, rig, ball_center
 
-# Create a group to act as the rig for the ball
-rig = cmds.group(empty=True, name='ball_rig')
+def apply_squash_deformer(rig):
+    """Apply the 'squash' deformer to the rig."""
+    squash = cmds.nonLinear(rig, type='squash')
+    cmds.parent(squash[1], rig)
+    return squash
 
-# Add the ball to the rig
-cmds.parent(ball, rig)
+def set_initial_position(rig):
+    """Set initial position of the rig."""
+    cmds.setAttr(rig + '.translateY', 20)
+    cmds.setAttr(rig + '.translateX', -10)
 
-# Get the center of the ball
-ball_center = cmds.xform(ball, query=True, worldSpace=True, rotatePivot=True)
+def move_squash_pivot(squash, ball_center):
+    """Move the pivot point of the squash deformer to the bottom of the ball."""
+    cmds.xform(squash[0], worldSpace=True, pivots=[ball_center[0], ball_center[1]-1, ball_center[2]])
 
-# Set the pivot point of the rig to the center of the ball
-cmds.xform(rig, worldSpace=True, pivots=ball_center)
+def set_falling_and_bouncing_keyframes(rig, squash, bounce_times, bounce_heights, squash_factors):
+    """Set keyframes for the rig's Y position to simulate falling and bouncing."""
+    fall_time = 45 * 0.8 * 0.25
+    cmds.setKeyframe(rig, attribute='translateY', time=0, value=20)
+    cmds.setKeyframe(rig, attribute='translateY', time=fall_time, value=0)
+    cmds.setKeyframe(squash[0], attribute='factor', time=0, value=0)
+    cmds.setKeyframe(squash[0], attribute='factor', time=fall_time, value=0)
 
-# Apply the 'squash' deformer to the rig
-squash = cmds.nonLinear(rig, type='squash')
+    for i in range(len(bounce_times) - 1):
+        peak_time = (bounce_times[i] + bounce_times[i+1]) / 2
+        cmds.setKeyframe(rig, attribute='translateY', time=bounce_times[i], value=0)
+        cmds.setKeyframe(rig, attribute='translateY', time=peak_time, value=bounce_heights[i+1])
+        cmds.setKeyframe(squash[0], attribute='factor', time=bounce_times[i], value=squash_factors[i])
+        cmds.setKeyframe(squash[0], attribute='factor', time=peak_time, value=0)
+        cmds.keyTangent(squash[0], attribute='factor', time=(bounce_times[i],), inTangentType='linear', outTangentType='linear')
 
-# Parent the deformer handle to the rig so it follows the rig's movement
-cmds.parent(squash[1], rig)
+    cmds.setKeyframe(rig, attribute='translateY', time=bounce_times[-1], value=0)
+    cmds.setKeyframe(squash[0], attribute='factor', time=bounce_times[-1], value=squash_factors[-1])
+    cmds.setKeyframe(squash[0], attribute='factor', time=bounce_times[-1] + 45*0.8*0.25, value=0)
 
-# Set initial position
-cmds.setAttr(rig + '.translateY', 20)  # Start from a higher position
-cmds.setAttr(rig + '.translateX', -10)  # Start from a more negative X position
+def set_forward_movement_keyframes(rig):
+    """Set keyframes for the rig's X position to simulate forward movement."""
+    cmds.setKeyframe(rig, attribute='translateX', time=0, value=-10)
+    cmds.setKeyframe(rig, attribute='translateX', time=240, value=10)
 
-# Move the pivot point of the squash deformer to the bottom of the ball
-cmds.xform(squash[0], worldSpace=True, pivots=[ball_center[0], ball_center[1]-1, ball_center[2]])
+def set_rotation_keyframes(rig, bounce_times, rotation_amount):
+    """Set keyframes for the rig's rotation to simulate rotation during the falling and in-air phases."""
+    for i in range(len(bounce_times) - 1):
+        peak_time = (bounce_times[i] + bounce_times[i+1]) / 2
+        cmds.setKeyframe(rig, attribute='rotateZ', time=bounce_times[i], value=i*rotation_amount)
+        cmds.setKeyframe(rig, attribute='rotateZ', time=peak_time, value=(i+0.5)*rotation_amount)
+    cmds.setKeyframe(rig, attribute='rotateZ', time=bounce_times[-1], value=(len(bounce_times)-1)*rotation_amount)
 
-# Set keyframes for the rig's Y position to simulate falling and bouncing
-fall_time = 45 * 0.8 * 0.25
-bounce_times = [fall_time, fall_time + 54*0.8*0.25, fall_time + 108*0.8*0.25, fall_time + 162*0.8*0.25, fall_time + 216*0.8*0.25]
-bounce_heights = [10, 8, 6, 4, 2]
+def set_continued_movement_and_rotation_keyframes(rig, bounce_times, rotation_amount):
+    """Set keyframes for the rig to continue moving and rotating after the last bounce."""
+    end_time = bounce_times[-1] + 45*0.8*0.25
+    cmds.setKeyframe(rig, attribute='translateX', time=end_time, value=15)
+    cmds.setKeyframe(rig, attribute='rotateZ', time=end_time, value=(len(bounce_times)-1)*rotation_amount - 45)
 
-# Set keyframe for the start of the fall
-cmds.setKeyframe(rig, attribute='translateY', time=0, value=20)
-# Set keyframe for the end of the fall
-cmds.setKeyframe(rig, attribute='translateY', time=fall_time, value=0)
+def main():
+    bounce_times = [45 * 0.8 * 0.25 * i for i in range(1, 6)]
+    bounce_heights = [10, 8, 6, 4, 2]
+    squash_factors = [-0.3, 0.3, -0.3, 0.3, -0.3]
+    rotation_amount = -90
 
-# Set keyframe for the start and end of the animation for the 'squash' deformer's factor attribute
-cmds.setKeyframe(squash[0], attribute='factor', time=0, value=0)  # No squash at the start
-cmds.setKeyframe(squash[0], attribute='factor', time=fall_time, value=0)  # No squash at the end
+    ball, rig, ball_center = create_ball_and_rig()
+    squash = apply_squash_deformer(rig)
+    set_initial_position(rig)
+    move_squash_pivot(squash, ball_center)
+    set_falling_and_bouncing_keyframes(rig, squash, bounce_times, bounce_heights, squash_factors)
+    set_forward_movement_keyframes(rig)
+    set_rotation_keyframes(rig, bounce_times, rotation_amount)
+    set_continued_movement_and_rotation_keyframes(rig, bounce_times, rotation_amount)
 
-# Define squash factors for each bounce
-squash_factors = [-0.3, 0.3, -0.3, 0.3, -0.3]
-
-for i in range(len(bounce_times) - 1):
-    # Set keyframe for when the rig hits the ground
-    cmds.setKeyframe(rig, attribute='translateY', time=bounce_times[i], value=0)
-    # Set keyframe for the peak of the bounce
-    peak_time = (bounce_times[i] + bounce_times[i+1]) / 2
-    cmds.setKeyframe(rig, attribute='translateY', time=peak_time, value=bounce_heights[i+1])
-
-    # Set keyframes for the 'squash' deformer's factor attribute to create the 'squash' effect
-    cmds.setKeyframe(squash[0], attribute='factor', time=bounce_times[i], value=squash_factors[i])  # Full squash
-    cmds.setKeyframe(squash[0], attribute='factor', time=peak_time, value=0)  # No squash
-
-    # Break the tangents of the deformer's factor attribute at the keyframes where the ball hits the ground
-    cmds.keyTangent(squash[0], attribute='factor', time=(bounce_times[i],), inTangentType='linear', outTangentType='linear')
-
-# Set the final keyframe for when the rig hits the ground
-cmds.setKeyframe(rig, attribute='translateY', time=bounce_times[-1], value=0)
-cmds.setKeyframe(squash[0], attribute='factor', time=bounce_times[-1], value=squash_factors[-1])  # Full squash
-
-# Set keyframe for the 'squash' deformer's factor attribute to return to original shape at the end of the animation
-cmds.setKeyframe(squash[0], attribute='factor', time=bounce_times[-1] + 45*0.8*0.25, value=0)  # No squash at the end
-
-# Set keyframes for the rig's X position to simulate forward movement
-cmds.setKeyframe(rig, attribute='translateX', time=0, value=-10)
-cmds.setKeyframe(rig, attribute='translateX', time=240, value=10)
-
-# Set keyframes for the rig's rotation to simulate rotation during the falling and in-air phases
-rotation_amount = -90  # Amount of rotation in degrees
-for i in range(len(bounce_times) - 1):
-    # Set keyframe for rotation at the start of the fall
-    cmds.setKeyframe(rig, attribute='rotateZ', time=bounce_times[i], value=i*rotation_amount)
-    # Set keyframe for rotation at the peak of the bounce
-    peak_time = (bounce_times[i] + bounce_times[i+1]) / 2
-    cmds.setKeyframe(rig, attribute='rotateZ', time=peak_time, value=(i+0.5)*rotation_amount)
-
-# Set the final keyframes for the rig's rotation
-cmds.setKeyframe(rig, attribute='rotateZ', time=bounce_times[-1], value=(len(bounce_times)-1)*rotation_amount)
-
-# Set keyframes for the rig to continue moving after the last bounce
-end_time = bounce_times[-1] + 45*0.8*0.25 # Adjusted end_time to account for the last bounce
-cmds.setKeyframe(rig, attribute='translateX', time=end_time, value=15)  # Continue moving in the X direction
-
-# Set keyframes for the rig to continue rotating after the last bounce
-cmds.setKeyframe(rig, attribute='rotateZ', time=end_time, value=(len(bounce_times)-1)*rotation_amount - 45)  # Continue rotating in the -Z direction
+if __name__ == "__main__":
+    main()
